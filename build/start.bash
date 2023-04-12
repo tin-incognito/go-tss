ca98178c2bad5cf0603f8edc514c42a6d2dde36b
0xakk0r0kamui <hy@autonomous.nyc>
2023-04-12 15:21:14 +0700

    Update bash

--- build/start.bash
+++ build/start.bash
@@ -1,2 +1,2 @@
 #!/bin/sh
-echo $SIGNER_PASSWORD | /go/bin/tss --tss_addr :8080  --p2p_port 6668 -loglevel debug --bridge_signer_name $SIGNER_NAME --bridge_signer_password $SIGNER_PASSWORD
+echo $SIGNER_PASSWORD | /go/bin/tss --tss_addr :8080  --p2p_port 6668 -loglevel debug --bridge_signer_password $SIGNER_PASSWORD
